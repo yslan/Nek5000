b596217206def75a90c3a88e7f6eb3dbe4cf7bee
Kento Kaneko <kaneko2@illinois.edu>
2017-08-23 11:25:25 -0500

    modified cem_drift_newton.f to be compatible with Nek5000

--- core/cem_drift_newton.f
+++ core/cem_drift_newton.f
@@ -8,14 +8,13 @@ c     This subroutine is the main core of JFNK method which involves
 c     using variables cN and cP in DRIFT
       subroutine cem_drift_newton
 c-----------------------------------------------------------------------      
-      implicit none
       include 'SIZE'
       include 'TOTAL'
-      include 'DRIFT'   ! lcdim, cn
-      include 'POISSON' ! mult
       include 'NEWTON'
       include 'CTIMER'
       include 'RTIMER'
+
+      parameter (lcdim=1)
 
       real alpha,tt
       real tolNT,tolGMRES
@@ -23,18 +22,18 @@ c-----------------------------------------------------------------------
       real rnorm,rnorms(lcdim)
       real rinorm,rinorms(lcdim),ratio
       real fnorm,fnorms(lcdim)
-      real temp(lpts1)
+      real temp(lx1*ly1*lz1*lelt)
 
       integer i,j,k,n,ic
       integer maxit
       real glsum
       real f_pre,f_now
       real max_dtNT,min_tol      
-      real sk  (lpts1,lcdim),  !
-     $     fk  (lpts1,lcdim),  !
-     $     gk  (lpts1,lcdim),  !
-     $     cn_n(lpts1,lcdim),  ! working array in Pseudo time step
-     $     cn_k(lpts1,lcdim)   ! working array in Newton iteration
+      real sk  (lx1*ly1*lz1*lelt,lcdim),  !
+     $     fk  (lx1*ly1*lz1*lelt,lcdim),  !
+     $     gk  (lx1*ly1*lz1*lelt,lcdim),  !
+     $     cn_n(lx1*ly1*lz1*lelt,lcdim),  ! working array in Pseudo time step
+     $     cn_k(lx1*ly1*lz1*lelt,lcdim)   ! working array in Newton iteration
 
 C     Parametar settings ToDo: move to user file, Lan
 
@@ -66,14 +65,14 @@ C     Initialization
         call rzero(cn_k(1,ic),npts) ! working array in NT
       enddo
       
-      call compute_f(fk,cn_k,lcdim,npts)
+      call compute_f_nt(fk,cn_k,lcdim,npts)
       call compute_gk(gk,fk,cn_k,cn_n,lcdim,npts) ! compute rhs g_k for GMRES
       do ic = 1,lcdim
         call chsign (gk(1,ic),npts)
       enddo
 
       do ic = 1,lcdim
-        fnorms(ic) = glsc3(fk(1,ic),fk(1,ic),mult,npts)
+        fnorms(ic) = glsc3(fk(1,ic),fk(1,ic),vmult,npts)
         fnorms(ic) = sqrt(fnorms(ic))
       enddo
       fnorm = glmax(fnorms,lcdim)
@@ -83,7 +82,7 @@ C     Initialization
 C     Start pseudo-time step
       do istep = 1,nsteps 
 
-        cpu_dtime = dclock()
+c       cpu_dtime = dclock() !FIXME
 
 C       SER, dt, dtNT varying
         if ((istep .eq. 1)) then
@@ -105,20 +104,20 @@ C       Start Newton iteration
 
           do ic = 1,lcdim ! solving newton sk
             call drift_hmh_gmres_newton(sk(1,ic),gk(1,ic)
-     $           ,cn_k,fk(1,ic),mult,lcdim,npts,tolGMRES,ic)
+     $           ,cn_k,fk(1,ic),vmult,lcdim,npts,tolGMRES,ic)
           enddo
           do ic = 1,lcdim ! update Newton iteration
             call add2s2(cn_k(1,ic),sk(1,ic),alpha,npts) ! cp_k=cp_k+alpha*sp_k
           enddo
 
-          call compute_f(fk,cn_k,lcdim,npts)
+          call compute_f_nt(fk,cn_k,lcdim,npts)
           call compute_gk(gk,fk,cn_k,cn_n,lcdim,npts)  ! checking tol. + next NT iter
           do ic = 1,lcdim
             call chsign(gk(1,ic),npts)
           enddo
 
           do ic = 1,lcdim
-            rnorms(ic) = glsc3(gk(1,ic),gk(1,ic),mult,npts)
+            rnorms(ic) = glsc3(gk(1,ic),gk(1,ic),vmult,npts)
             rnorms(ic) = sqrt(rnorms(ic))
           enddo
           rnorm = glmax(rnorms,lcdim)
@@ -141,7 +140,7 @@ c          if (rnorm.lt.new_tol) goto 900 ! Newton conv
 
 c       Compute the norm of f
         do ic = 1,lcdim
-          fnorms(ic) = glsc3(fk(1,ic),fk(1,ic),mult,npts)  
+          fnorms(ic) = glsc3(fk(1,ic),fk(1,ic),vmult,npts)  
           fnorms(ic) = sqrt(fnorms(ic))
         enddo
         fnorm  = glmax(fnorms,lcdim)
@@ -149,7 +148,7 @@ c       Compute the norm of f
         f_now = fnorm  ! set up new fnorm to f_now
 
 c       Compute the CPU_time
-        cpu_dtime = dclock()-cpu_dtime
+c       cpu_dtime = dclock()-cpu_dtime !FIXME
         cpu_t = cpu_t+cpu_dtime
         cpu_t_step = cpu_t/istep
         cpu_p_t = glsum(cpu_t_step /npts,1)/np
@@ -157,15 +156,15 @@ c       Compute the CPU_time
         time = time + dt
 
 C       Copy for cem_out and cem_chk
-        do ic = 1,lcdim
-          call copy(cN(1,ic),cn_k(1,ic),npts) ! just for cem_out
-        enddo
-        call userchk
-        call cem_out
+c       do ic = 1,lcdim
+c         call copy(cN(1,ic),cn_k(1,ic),npts) ! just for cem_out
+c       enddo
+c       call userchk
+c       call cem_out
       
       enddo
       
-      call cem_end
+c     call cem_end !FIXME
 
       return
       end
@@ -180,21 +179,27 @@ c     Input    f
 c     Output   gout
       subroutine compute_gk(gout,f,ckn,c0n,nion,n)
 c-----------------------------------------------------------------------      
-      implicit none
       include 'SIZE'
       include 'TOTAL'
       include 'NEWTON'
       integer nion,n,ic
-      real gout(lpts1,nion)
-     $   , ckn (lpts1,nion)
-     $   , c0n (lpts1,nion)
-     $   , f   (lpts1,nion)
+      real gout(lx1*ly1*lz1*lelt,nion)
+     $   , ckn (lx1*ly1*lz1*lelt,nion)
+     $   , c0n (lx1*ly1*lz1*lelt,nion)
+     $   , f   (lx1*ly1*lz1*lelt,nion)
 
-      do ic = 1,nion
-        call add3s3(gout(1,ic),ckn(1,ic),f(1,ic),c0n(1,ic)
-     $       ,1.0,-1.0*dtNT,-1.0,n)
-        call cmult(gout(1,ic),dtNTinv,n)   ! case with divided by dt_newton
-      enddo
+c     do ic = 1,nion
+c       call add3s3(gout(1,ic),ckn(1,ic),f(1,ic),c0n(1,ic)
+c    $       ,1.0,-1.0*dtNT,-1.0,n)
+
+      ic = 1
+
+c     call sub3s2(gout,ckn,f,1.,-dtnt,n)
+      call add3s2(gout,ckn,f,-1.-dtnt,n)
+
+      call sub2(gout,c0n,n)
+      call cmult(gout(1,ic),dtNTinv,n)   ! case with divided by dt_newton
+c     enddo
 
       return
       end
@@ -204,17 +209,15 @@ c     BDF1, it can be changed to BDF2 or so on.
 c  
 c       f(u) = 1/dt ( \tilde{u} - u ),   \tilde{u} = BDF1(u)
 c
-      subroutine compute_f(fout,cin,nion,n)
+      subroutine compute_f_nt(fout,cin,nion,n)
 c-----------------------------------------------------------------------
-      implicit none
       include 'SIZE'
       include 'TOTAL'
       include 'NEWTON'
-      include 'POISSON' 
       integer nion,n,ic
-      real fout(lpts1,nion) ! f output
-      real cin (lpts1,nion) ! c input
-      real cout(lpts1,nion) ! c output (sem_bdf)
+      real fout(lx1*ly1*lz1*lelt,nion) ! f output
+      real cin (lx1*ly1*lz1*lelt,nion) ! c input
+      real cout(lx1*ly1*lz1*lelt,nion) ! c output (sem_bdf)
 
       call cem_drift_sem_bdf1_newton(cin,cout,nion,n,0)
 
@@ -234,27 +237,25 @@ c        J_k s_k = s_k - (dt /eps) *  ( f(u_k + eps*s_k) - f(u_k) )
 c   
 c     where f(u_k) has been store in each Newton iteration
 c           f(u_k + eps*s_k) has to be computed in each GMRES iteration
-      subroutine JacobiMatVec(Jp,p,uc,fi,nion,n,iflag)
+      subroutine JacobiMatVec(jacp,p,uc,fi,nion,n,iflag)
 c-----------------------------------------------------------------------
-      implicit none
       include 'SIZE'
       include 'TOTAL'
       include 'NEWTON'
-      include 'POISSON'
 
-      real     Jp (lpts1),p(lpts1)
-      real     uc (lpts1,nion)     ! treat other ions as variable
-      real     fi (lpts1)          ! f_in
-      real     fo (lpts1,nion)     ! f_out (multi-ions)
-      real     foi(lpts1)          ! f_out - f_in (restricted on iflag)
-      real     uep(lpts1,nion)     ! uc + eps p
+      real     jacp(lx1*ly1*lz1*lelt),p(lx1*ly1*lz1*lelt)
+      real     uc (lx1*ly1*lz1*lelt,nion)     ! treat other ions as variable
+      real     fi (lx1*ly1*lz1*lelt)          ! f_in
+      real     fo (lx1*ly1*lz1*lelt,nion)     ! f_out (multi-ions)
+      real     foi(lx1*ly1*lz1*lelt)          ! f_out - f_in (restricted on iflag)
+      real     uep(lx1*ly1*lz1*lelt,nion)     ! uc + eps p
       integer  nion,n,iflag,ic
       real     eps,pnorm,unorm,glsc3 
 
 C     Determine eps with norms of p and u_iflag
-      pnorm = glsc3(p,p,mult,n)
+      pnorm = glsc3(p,p,vmult,n)
       pnorm = sqrt(pnorm)
-      unorm = glsc3(uc(1,iflag),uc(1,iflag),mult,n)
+      unorm = glsc3(uc(1,iflag),uc(1,iflag),vmult,n)
       unorm = sqrt(unorm)
 
       eps = (1+unorm)*1e-14   ! formula for varing eps
@@ -268,12 +269,12 @@ C     Compute Jp
       enddo
       call add3s2(uep(1,iflag),uc(1,iflag),p,1.0,eps,n) ! uep = u + eps*p
 
-      call compute_f(fo,uep,nion,n)
+      call compute_f_nt(fo,uep,nion,n)
 
       call sub3  (foi,fo(1,iflag),fi,n)
       call cmult (foi,epsinv*dtNT,n) ! foi = (fo-fi)*dt_newton/eps
-      call sub3  (Jp,p,foi,n)
-      call cmult (Jp,dtNTinv,n)      ! Jp = p/dt_newton - (fo-f)/eps
+      call sub3  (Jacp,p,foi,n)
+      call cmult (Jacp,dtNTinv,n)      ! Jp = p/dt_newton - (fo-f)/eps
                                      ! case with divided by dt_newton
       return
       end
@@ -289,9 +290,10 @@ c     GMRES iteration.
       include 'GMRES'
 
       integer  n,nion,outer,iflag
-      real     phi(lpts1),res(lpts1),wt(lpts1)
+      real     phi(lx1*ly1*lz1*lelt),res(lx1*ly1*lz1*lelt),
+     $         wt(lx1*ly1*lz1*lelt)
       real     tol,alpha,l,temp
-      real     eps,uc(lpts1,nion),f(lpts1)
+      real     eps,uc(lx1*ly1*lz1*lelt,nion),f(lx1*ly1*lz1*lelt)
       real*8   etime1,dnekclock
 
 c      if (nid.eq.0) write(6,*) 'start: hmh_gmres'
@@ -310,59 +312,61 @@ c      if (nid.eq.0) write(6,*) 'start: hmh_gmres'
       do while (iconv.eq.0.and.iter.lt.500)
          outer = outer+1
          if(iter.eq.0) then
-            call copy  (r,res,n)                  ! r = res
+            call copy  (r_gmres,res,n)                  ! r = res
          else
             !update residual
-            call copy   (r,res,n)                  ! r = res
-            call JacobiMatVec(w,x,uc,f,nion,n,iflag)! w = A x
-            call add2s2 (r,w,-1.,n) ! r = r - w
+            call copy   (r_gmres,res,n)                  ! r = res
+            call JacobiMatVec(w_gmres,x_gmres,uc,f,nion,n,iflag)! w = A x
+            call add2s2 (r_gmres,w_gmres,-1.,n) ! r = r - w
          endif
 
-         gamma(1) = glsc3(r,r,wt,n)                ! gamma  = (r,r)
-         gamma(1) = sqrt(gamma(1))                 ! gamma  = sqrt{ (r,r) }
+         gamma_gmres(1) = glsc3(r_gmres,r_gmres,wt,n)                ! gamma_gmres  = (r,r)
+         gamma_gmres(1) = sqrt(gamma_gmres(1))                 ! gamma_gmres  = sqrt{ (r,r) }
 
-         tolps = 0.1*gamma(1)  ! tolerance for gmres                   
+         tolps = 0.1*gamma_gmres(1)  ! tolerance for gmres                   
          tolpss = tolps        ! by using inexact Newton method
                                ! 0.1 for forcing term and is changable
 
          !check for lucky convergence
          rnorm = 0.
-         if(gamma(1) .eq. 0.) goto 9000
-         temp = 1./gamma(1)
-         call cmult2(v(1,1),r,temp,n)             !  v  = r / gamma
+         if(gamma_gmres(1) .eq. 0.) goto 9000
+         temp = 1./gamma_gmres(1)
+         call cmult2(v_gmres(1,1),r,temp,n)             !  v  = r / gamma_gmres
                                                   !  1            1
          !write(6,*) 'start form m-th krylov subspace'
          do j=1,m
             iter = iter+1
 
-            call JacobiMatVec(w,v(1,j),uc,f,nion,n,iflag) ! w = A v
+            call JacobiMatVec(w_gmres,v_gmres(1,j),uc,f,nion,n,iflag) ! w = A v
 
             !modified Gram-Schmidt
             do i=1,j
-               h(i,j)=glsc3(w,v(1,i),wt,n)        ! h    = (w,v )
+               h_gmres(i,j)=glsc3(w_gmres,v_gmres(1,i),wt,n)        ! h    = (w,v )
                                                   ! i,j       i
-               call add2s2(w,v(1,i),-h(i,j),n)    ! w = w - h    v
+               call add2s2(w_gmres,v_gmres(1,i),-h_gmres(i,j),n)    ! w = w - h    v
             enddo                                 !         i,j  i
 
 
             !apply Givens rotations to new column
             do i=1,j-1
-               temp = h(i,j)
-               h(i  ,j)=  c(i)*temp + s(i)*h(i+1,j)
-               h(i+1,j)= -s(i)*temp + c(i)*h(i+1,j)
+               temp = h_gmres(i,j)
+               h_gmres(i  ,j)=  c_gmres(i)*temp 
+     $         + s_gmres(i)*h_gmres(i+1,j)
+               h_gmres(i+1,j)= -s_gmres(i)*temp 
+     $         + c_gmres(i)*h_gmres(i+1,j)
             enddo
                                               !            ______
             alpha = sqrt(glsc3(w,w,wt,n))     ! alpha =  \/ (w,w)
             if(alpha.eq.0.) goto 900 !converged
-            l = sqrt(h(j,j)*h(j,j)+alpha*alpha)
+            l = sqrt(h_gmres(j,j)*h_gmres(j,j)+alpha*alpha)
             temp = 1./l
-            c(j) = h(j,j) * temp
-            s(j) = alpha  * temp
-            h(j,j) = l
-            gamma(j+1) = -s(j) * gamma(j)
-            gamma(j)   =  c(j) * gamma(j)
+            c_gmres(j) = h_gmres(j,j) * temp
+            s_gmres(j) = alpha  * temp
+            h_gmres(j,j) = l
+            gamma_gmres(j+1) = -s_gmres(j) * gamma_gmres(j)
+            gamma_gmres(j)   =  c_gmres(j) * gamma_gmres(j)
 
-            rnorm = abs(gamma(j+1))
+            rnorm = abs(gamma_gmres(j+1))
 
 c            if ((nid.eq.0).and.(istep.le.2))
 c     $           write (6,66) iter,tolpss,rnorm,istep
@@ -372,7 +376,7 @@ c     $           write (6,66) iter,tolpss,rnorm,istep
             if (j.eq.m) goto 1000 !not converged, restart
 
             temp = 1./alpha
-            call cmult2(v(1,j+1),w,temp,n)   ! v    = w / alpha
+            call cmult2(v_gmres(1,j+1),w_gmres,temp,n)   ! v    = w / alpha
                                              !  j+1
          enddo
 c        write(6,*) 'end of forming m-th krylov subspace'
@@ -381,24 +385,24 @@ c        write(6,*) 'end of forming m-th krylov subspace'
 
 c        back substitution
 c             -1
-c        c = H   gamma
+c        c = H   gamma_gmres
 c        write(6,*) 'start solving least squre problem'
          do k=j,1,-1
-            temp = gamma(k)
+            temp = gamma_gmres(k)
             do i=j,k+1,-1
-               temp = temp - h(k,i)*c(i)
+               temp = temp - h_gmres(k,i)*c_gmres(i)
             enddo
-            c(k) = temp/h(k,k)
+            c_gmres(k) = temp/h_gmres(k,k)
          enddo
          !sum up Arnoldi vectors
          do i=1,j
-            call add2s2(x,v(1,i),c(i),n)     ! x = x + c  z
+            call add2s2(x_gmres,v_gmres(1,i),c_gmres(i),n)     ! x = x + c  z
          enddo                               !          i  i
 c     write(6,*) 'end of solving least squre problem'
       enddo
  9000 continue
 
-      call copy(phi,x,n)
+      call copy(phi,x_gmres,n)
 
 c     call ortho   (res) ! Orthogonalize wrt null space, if present
 
@@ -416,31 +420,30 @@ C     This subroutine compute sem_bdf1, user can provide any(*)
 C     bdf1 time swapper in here
       subroutine cem_drift_sem_bdf1_newton(cinput,coutput,nion,n,iflag)
 c---------------------------------------------------------------------
-      implicit none
       include 'SIZE'
       include 'TOTAL'
-      include 'DRIFT' ! cn
       include 'ZPER'
-      include 'BCS'
+c     include 'BCS'
 
       integer iflag,nion,n,ic
-      real cinput(lpts1,nion)
-      real coutput(lpts1,nion)
+      real cinput(lx1*ly1*lz1*lelt,nion)
+      real coutput(lx1*ly1*lz1*lelt,nion)
 
 
 C     Copy into working array of sem_bdf1
-      do ic = 1,lcdim
-        call copy(cN(1,ic),cinput(1,ic),n)
+      do ic = 1,nion
+c       call copy(cN(1,ic),cinput(1,ic),n)
       enddo
 
 C     Call sem_bdf1
 C     vvvv Put sem_bdf subroutine here
-      call cem_drift_op_bdf
+
+c     call cem_drift_op_bdf !FIXME
 
 
 C     Copy out working array of sem_bdf1
-      do ic = 1,lcdim
-        call copy(coutput(1,ic),cN(1,ic),n)
+      do ic = 1,nion
+c       call copy(coutput(1,ic),cN(1,ic),n)
       enddo
 
       return
